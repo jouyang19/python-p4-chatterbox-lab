d04f4ac39c702a6668a99aff0f1c3c993dc70004
Guang <jouyang19@proton.me>
2024-06-26 14:46:09 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,58 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+# messages: get, post
+# messages/id: patch, delete
 
-@app.route('/messages/<int:id>')
+@app.route('/messages', methods = ('GET', 'POST'))
+def messages():
+    if request.method == "GET":
+        
+        msgs = Message.query.order_by(Message.created_at).all()
+        
+        return [m.to_dict() for m in msgs], 200
+    
+    if request.method == "POST":
+        
+        data = request.get_json()
+        
+        new_message = Message(
+            body = data.get('body'),
+            username = data.get('username')
+        )
+        
+        db.session.add(new_message)
+        db.session.commit()
+        
+        return new_message.to_dict(), 201
+
+@app.route('/messages/<int:id>', methods = ('PATCH', 'DELETE'))
 def messages_by_id(id):
-    return ''
+    
+    msg = Message.query.filter(Message.id == id).first()
+    
+    if not msg:
+        return {"error": "message not found"}, 404
+    
+    if request.method == "PATCH":
+        
+        data = request.get_json()
+        
+        for field in data:
+            setattr(msg, field, data[field])
+            
+        db.session.add(msg)
+        db.session.commit()
+        
+        return msg.to_dict(), 201
+    
+    elif request.method == "DELETE":
+        
+        db.session.delete(msg)
+        db.session.commit()
+        
+        return {}, 204
+        
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -8,7 +8,17 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+#id, body, username, created_at (datetime)
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, default = "")
+    username = db.Column(db.String, default="Duane")
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    
+    def __repr__(self):
+        return f'<Message {self.id} from {self.username} c: {self.created_at} u: {self.updated_at}: {self.body}>'
+    
+    
